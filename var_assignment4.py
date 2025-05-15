6180cb390a1ba079c3b7d38fef974449492aa791
maxrux <maxrux77@gmail.com>
2025-05-15 15:07:05 +0200

    ggggg

--- var_assignment4.py
+++ var_assignment4.py
@@ -5,3 +5,17 @@ x, y, z = fruits
 print(x)
 print(y)
 print(z,y)
+
+print("######################")
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+
+print("#########################")
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
