c7e2d5a823482342aa41464d5e06fda6c95df8ca
maxrux <maxrux77@gmail.com>
2025-05-20 16:02:21 +0200

    hhhh

---
+++ numbers_variables.py
@@ -0,0 +1,13 @@
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+import random
+print("Random number:")
+print(random.randrange(1, 1000))
